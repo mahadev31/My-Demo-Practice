a7eaf82f39d70806c0a35009d638ab956815d3be
sanjaydangar22 <sanjaydangar31@gmail.com>
2023-08-15 07:29:32 +0530

    add

---
+++ SqliteDatabase/app/src/main/java/com/demo/sqlitedatabase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.sqlitedatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
