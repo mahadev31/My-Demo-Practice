d27f908771e97eb551823bccd97a92f5c1cc7d73
sanjaydangar22 <sanjaydangar2212@gmail.com>
2023-08-17 22:40:34 +0530

    add

---
+++ ResumeSqlite/app/src/main/java/com/demo/resumesqlite/DisplayDataActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.resumesqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_data)
+    }
+}
--- ResumeSqlite/app/src/main/java/com/demo/resumesqlite/FirstPageActivity.kt
+++ ResumeSqlite/app/src/main/java/com/demo/resumesqlite/FirstPageActivity.kt
@@ -2,10 +2,30 @@ package com.demo.resumesqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo.resumesqlite.databinding.ActivityFirstPageBinding
 
 class FirstPageActivity : AppCompatActivity() {
+
+    lateinit var firstPageBinding: ActivityFirstPageBinding
+    companion object {
+        var firstName: String? = null
+        var lastName: String? = null
+        var mobileNumber: String? = null
+        var address: String? = null
+        var dd: String? = null
+        var mm: String? = null
+        var yy: String? = null
+        var gender: String? = null
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_page)
+        firstPageBinding= ActivityFirstPageBinding.inflate(layoutInflater)
+        setContentView(firstPageBinding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+
     }
 }
