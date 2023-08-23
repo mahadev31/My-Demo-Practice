bf39bfd67d6ed7e1163e0510690af891f4b7f29b
sanjaydangar22 <sanjaydangar2212@gmail.com>
2023-08-23 10:52:01 +0530

    add

--- wallpaperApi/app/src/main/java/com/demo/wallpaperapi/DisplayActivity.kt
+++ wallpaperApi/app/src/main/java/com/demo/wallpaperapi/DisplayActivity.kt
@@ -2,6 +2,7 @@ package com.demo.wallpaperapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bumptech.glide.Glide
 import com.demo.wallpaperapi.databinding.ActivityDisplayBinding
 
@@ -9,17 +10,18 @@ class DisplayActivity : AppCompatActivity() {
     lateinit var displayBinding: ActivityDisplayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        displayBinding=ActivityDisplayBinding.inflate(layoutInflater)
+        displayBinding = ActivityDisplayBinding.inflate(layoutInflater)
         setContentView(displayBinding.root)
 
         initView()
     }
 
     private fun initView() {
-        var url=intent.getStringExtra("url")
+        var url = intent.getStringExtra("url")
 
+        Log.e("TAG", "initView: " + url)
         Glide.with(this)
-            .load("https://api.slingacademy.com/public/sample-photos/$url")
+            .load(url)
             .placeholder(
                 R.drawable.ic_image
             ).into(displayBinding.imgView)
